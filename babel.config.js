4991df227c16bbba403c065a395670a36a9551f9
yubowei982 <yubowei0982@phytium.com.cn>
2023-09-05 11:16:21 +0800

    Import element-ui into webui-vue
    Signed-off-by: yubowei982 <yubowei0982@phytium.com.cn>
    Change-Id: I918616b291fcfb05c117fd8480b9ca8f2c5db169

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [['@vue/cli-plugin-babel/preset', { useBuiltIns: 'entry' }]],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk',
+      },
+    ],
+  ],
   env: {
     test: {
       plugins: ['transform-require-context'],
