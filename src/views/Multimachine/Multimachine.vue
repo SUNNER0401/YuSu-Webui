df2fcca435e2e25cabc4fc31f4b37e8cee2fd89a
SUNNER0401 <470425259@qq.com>
2024-06-25 18:05:09 +0800

    机箱管理v1—无循环、无数据绑定

--- src/views/Multimachine/Multimachine.vue
+++ src/views/Multimachine/Multimachine.vue
@@ -1,13 +1,52 @@
 <template>
   <b-container fluid>
+    <!-- 页面标题 -->
     <page-title />
-    <overview-quick-links class="mb-4" />
-    <page-section section-title="第一部分" class="mb-1">
-      <h3 class="h5 mb-0">这里是第一部分</h3>
-    </page-section>
-    <page-section section-titile="第二部分">
-      <h3 class="h5 mb-0">这里是第二部分</h3>
-    </page-section>
+    <b-card-group>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+      <multimachine-card></multimachine-card>
+    </b-card-group>
+    <!-- <b-card-group>
+      <div v-for="(value,key)in MultimachineCard" :key="key">
+        <multimachine-card :data="value" :title="key"> </multimachine-card>
+      </div>
+    </b-card-group> -->
   </b-container>
 </template>
-<script></script>
+<script>
+import PageTitle from '@/components/Global/PageTitle';
+import MultimachineCard from './MultimachineCard';
+import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
+// import MultimachineCard from './MultimachineCard.vue';
+export default {
+  name: 'Multimachine',
+  components: {
+    PageTitle,
+    MultimachineCard,
+  },
+  mixins: [LoadingBarMixin],
+  //离开当前路由之前做的一些工作
+  // beforeRouteLeave (to, from, next) {
+  //   clearInterval(this.timer);
+  //   this.hideLoader();
+  //   next();
+  // },
+  // data() {
+  //   return {};
+  // },
+  // computed: {
+  //   MultimachineCard() {
+  //     return this.$store.getters['Multimachine/'];
+  //   },
+  // },
+};
+</script>
---
+++ src/views/Multimachine/MultimachineCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-card bg-variant="light" border-variant="light" class="mb-4">
+    <!-- 标题 -->
+    <div class="justify-content-between align-items-center d-flex flex-wrap">
+      <!-- 卡片标题 -->
+      <h3 class="h5 mb-0">路由001</h3>
+      <!-- 查看更多按钮 -->
+      <b-link :to="to">{{ $t('pageMultimachine.viewMore') }}</b-link>
+    </div>
+    <!-- 内容 -->
+    <b-row class="mt-3">
+      <b-col sm="6">
+        <dl>
+          <!-- 型号 -->
+          <dt>{{ $t('pageMultimachine.model') }}</dt>
+          <dd>{{ dataFormatter(model) }}</dd>
+          <!-- 序列号 -->
+          <dt>{{ $t('pageMultimachine.serialNumber') }}</dt>
+          <dd>{{ dataFormatter(serialNumber) }}</dd>
+          <!-- ip -->
+          <dt>{{ $t('pageMultimachine.ipAddress') }}</dt>
+          <dd>{{ dataFormatter(ipAddress) }}</dd>
+          <!-- 健康状态 -->
+          <dt>{{ $t('pageMultimachine.health') }}</dt>
+          <dd>{{ dataFormatter(health) }}</dd>
+        </dl>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+<script>
+import DataFormatterMixin from '@/components/Mixins/DataFormatterMixin';
+export default {
+  name: 'MultimachineCard',
+  mixins: [DataFormatterMixin],
+};
+</script>
+<style lang="scss" scoped>
+a {
+  vertical-align: middle;
+  font-size: 14px;
+}
+
+.card {
+  min-width: 300px;
+  max-width: 330px;
+  margin-right: 20px;
+  margin-left: 10px;
+}
+</style>
