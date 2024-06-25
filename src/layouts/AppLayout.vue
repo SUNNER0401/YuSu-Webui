de5211c969eeebd5378e6fd8f2f33ac885be8ba9
littlebee21 <805876520@qq.com>
2024-06-25 11:04:43 +0800

    AppLayout修改机箱管理为全屏模式

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -10,8 +10,10 @@
         :class="{
           isFullScreen:
             $route.path === '/' ||
+            $route.path === '/chassis-management' ||
             $route.name === 'page-not-found' ||
-            $route.path === '/profile-settings',
+            $route.path === '/profile-settings' ||
+            $route.path === '/multimachine',
         }"
       >
         <page-container class="app-content">
