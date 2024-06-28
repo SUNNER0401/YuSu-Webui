a576e1e62febf1ca2bb52711c8efe06df3e8303c
SUNNER0401 <470425259@qq.com>
2024-06-28 18:03:19 +0800

    MachineDetails中Tab选项框初步搭建

--- src/views/MachineDetails/MachineDetails.vue
+++ src/views/MachineDetails/MachineDetails.vue
@@ -1,5 +1,39 @@
 <template>
-  <page-title />
+  <b-container fluid>
+    <page-title />
+    <div>
+      <ul class="nav nav-tabs" role="tablist">
+        <li
+          v-for="(tab, index) in tabs"
+          :key="index"
+          class="nav-item"
+          :class="{ active: currentTab === tab.id }"
+        >
+          <a
+            class="nav-link"
+            :href="'#' + tab.id"
+            role="tab"
+            data-toggle="tab"
+            @click.prevent="changeTab(tab.id)"
+          >
+            {{ tab.title }}
+          </a>
+        </li>
+      </ul>
+
+      <div class="tab-content">
+        <div
+          v-for="(tab, index) in tabs"
+          :id="tab.id"
+          :key="index"
+          class="tab-pane fade"
+          :class="{ 'show active': currentTab === tab.id, fade: true }"
+        >
+          <p>{{ tab.content }}</p>
+        </div>
+      </div>
+    </div>
+  </b-container>
 </template>
 <script>
 import PageTitle from '@/components/Global/PageTitle';
@@ -10,5 +44,35 @@ export default {
     PageTitle,
   },
   mixins: [LoadingBarMixin],
+  data() {
+    return {
+      currentTab: 'tab1',
+      tabs: [
+        {
+          id: 'tab1',
+          title: '页面1',
+          content: 'slahlkasdfnak',
+        },
+        {
+          id: 'tab2',
+          title: '页面2',
+          content: 'lalallala',
+        },
+      ],
+    };
+  },
+  methods: {
+    changeTab(tabId) {
+      this.currentTab = tabId;
+    },
+  },
 };
 </script>
+<style>
+.nav-item {
+  background-color: aliceblue;
+}
+.tab-pane fade {
+  background-color: antiquewhite;
+}
+</style>
