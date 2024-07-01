33c4efc0079687fca3a149dd7b52622d13ad3584
SUNNER0401 <470425259@qq.com>
2024-07-01 18:01:46 +0800

    MachineDetails修改样式

--- src/views/MachineDetails/MachineDetails.vue
+++ src/views/MachineDetails/MachineDetails.vue
@@ -1,27 +1,65 @@
 <template>
   <b-container fluid>
-    <page-title />
-    <b-tabs v-model="activeTab">
-      <b-tab
-        v-for="(machine, key) in machines"
-        :key="key"
-        :title="machine.title"
-      >
-        <div>
-          <h4>{{ machine.title }}</h4>
-          <ul>
-            <li>型号: {{ machine.model }}</li>
-            <li>序列号: {{ machine.serialNumber }}</li>
-            <li>IP地址: {{ machine.ipAddress }}</li>
-            <li>健康状态: {{ machine.health }}</li>
-            <li>主机名: {{ machine.hostname }}</li>
-            <li>连接状态: {{ machine.linkStatus }}</li>
-            <li>警告信息: {{ machine.warning }}</li>
-            <li>错误信息: {{ machine.error }}</li>
-          </ul>
-        </div>
-      </b-tab>
-    </b-tabs>
+    <div class="d-flex justify-content-between align-items-center mb-3">
+      <page-title />
+      <b-button variant="secondary" @click="goBack">返回</b-button>
+    </div>
+    <b-card no-body>
+      <b-tabs v-model="activeTab" justified card>
+        <b-tab
+          v-for="(machine, key) in machines"
+          :key="key"
+          :title="machine.title"
+        >
+          <div>
+            <h4>{{ machine.title }}</h4>
+            <ul>
+              <li>型号: {{ machine.model }}</li>
+              <li>序列号: {{ machine.serialNumber }}</li>
+              <li>IP地址: {{ machine.ipAddress }}</li>
+              <li>
+                健康状态:
+                {{ machine.health }}
+                <svg-icon
+                  :icon-class="
+                    machine.health === 'yes' ? 'healthy' : 'unhealthy'
+                  "
+                />
+              </li>
+              <li>主机名: {{ machine.hostname }}</li>
+              <li>
+                连接状态: {{ machine.linkStatus
+                }}<svg-icon
+                  :icon-class="
+                    machine.linkStatus === 'fine' ? 'linkage-interrupt' : ''
+                  "
+                />
+              </li>
+              <li v-if="machine.warning !== '0'" style="color: orange">
+                警告信息: {{ machine.warning }}
+                <svg-icon icon-class="warning" />
+              </li>
+              <li v-else>警告信息: {{ machine.warning }}</li>
+              <li v-if="machine.error !== '0'" style="color: red">
+                错误信息: {{ machine.error }}
+                <svg-icon icon-class="error" />
+              </li>
+              <li v-else>错误信息: {{ machine.error }}</li>
+              <li>
+                机器状态:{{ machine.power }}<svg-icon icon-class="power-on" />
+                <svg-icon icon-class="power-off" />
+              </li>
+            </ul>
+          </div>
+        </b-tab>
+      </b-tabs>
+    </b-card>
+    <div class="text-center">
+      <b-button-group class="mt-2">
+        <b-button @click="activeTab--">前一项</b-button>
+        <b-button @click="activeTab++">后一项</b-button>
+      </b-button-group>
+    </div>
   </b-container>
 </template>
 <script>
@@ -205,6 +243,9 @@ export default {
         ...allMachines[key],
       }));
     },
+    goBack() {
+      this.$router.push('/multimachine');
+    },
   },
 };
 </script>
@@ -215,4 +256,31 @@ export default {
 .tab-pane fade {
   background-color: antiquewhite;
 }
+.card-header {
+  background: linear-gradient(138deg, #acb6e56b, #86fde83d);
+  border-bottom: 0px solid rgba(0, 0, 0, 0.125);
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.tab-content > .active {
+  font-size: 18px;
+}
+.card {
+  border-radius: 30px;
+}
+.card-header {
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+}
+.card-body {
+  border-bottom-left-radius: 30px;
+  border-bottom-right-radius: 30px;
+}
+.nav-tabs .nav-link.active,
+.nav-tabs .nav-item.show .nav-link {
+  color: #393737;
+  background-color: #91cfcaa8;
+  border-color: #aca2a2b0 #aca2a2b0 #1d0606;
+}
+/* 渐变切换效果 */
 </style>
