5fe1ea1bbb07dd912aa218b77c17d90ca134c9f3
SUNNER0401 <470425259@qq.com>
2024-07-05 11:55:38 +0800

    HeaderNavigation头部导航栏添加机箱管理路由

--- src/components/AppHeader/HeaderNavigation.ts
+++ src/components/AppHeader/HeaderNavigation.ts
@@ -12,6 +12,7 @@ export type NavItemType = {
 
 export const FatherName = {
   '/': i18n.t('appPageTitle.overview'),
+  multimachine: i18n.t('appPageTitle.multimachine'),
   'system-management': i18n.t('appNavigation.systemManagement'),
   diagnostic: i18n.t('appNavigation.diagnostic'),
   'user-security': i18n.t('appNavigation.UserSecurity'),
@@ -34,6 +35,16 @@ export default {
       component: () => import('@/views/Overview'),
       meta: {
         title: i18n.t('appPageTitle.overview'),
+      },
+    },
+  ],
+  multimachine: [
+    {
+      path: '/multimachine',
+      name: 'multimachine',
+      component: () => import('@/views/Multimachine'),
+      meta: {
+        title: i18n.t('appPageTitle.multimachine'),
       },
     },
   ],
